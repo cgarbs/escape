2f3423f1c21088c274ef9170562012a75e6a5378
cgarbs <christian.garba@gmail.com>
2021-08-26 20:04:55 -0700

    done

---
+++ escape/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                HOME
+                BOOK NOW
+                ABOUT US 
+                CONTACT
+            </div>
+        );
+    }
+}
+
+export default NavBar;
