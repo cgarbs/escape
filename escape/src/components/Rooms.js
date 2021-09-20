25715575d019fd280e4efe35028f59ad7cf00623
Christian <christian.garba@gmail.com>
2021-09-19 22:30:06 -0700

    done

--- escape/src/components/Rooms.js
+++ escape/src/components/Rooms.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 //Description of all rooms plus link to reservations.
+//Minimum of 6-8 rooms + descriptions.
 
 class Rooms extends Component {
     render() {
